4c0b60ba6ea0e2b539a34457523abe65b7762b1a
Trent Marshall <Trent@Sarahs-MacBook-Air-2.local>
2017-03-04 17:32:08 -0500

    js function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-alert("filestructure?");
-
 var apiKey = "api-key=12500f997ba043fca26ead8c403c8e7b";
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
@@ -10,16 +8,31 @@ var startYear = "";
 var endYear = "";
 
 $('button').on("click", function(){
-	alert("search button works");
 	searchTerm = $('#searchTerm').val();
+	console.log(searchTerm);
 	numRecords = $('#numRecords').val();
+	console.log(numRecords);
 	startYear = $('#startYear').val();
+	console.log(startYear);
 	endYear = $('#endYear').val();
+	console.log(endYear);
 
-	var newQuery = queryURL + "q=" + searchTerm + "&?page=" + numRecords + "&?begin_date=" + startYear + "&?end_date" + endYear + "&" + apiKey;
-	console.log(newQuery);
+	var newQuery = queryURL + "q=" + searchTerm + "&?begin_date=" + startYear + "&?end_date" + endYear + "&" + apiKey;
+	// "&?page=" + numRecords + 
 
-}
+	$.ajax({
+		url: newQuery,
+		method: "GET"
+
+	}).done(function(NYT) {
+	console.log(NYT);
+	var articleTitle = JSON.stringify(NYT.response.docs[0].headline.main);
+	console.log(articleTitle);
+	
+	$('#articles').append(articleTitle);
+	})
+
+});
 
 
 
